5c794284c33efddbe7a7e3454dcc4db822e090c5
iamgunbun <ammonsgunner@gmail.com>
2026-07-15 17:44:11 -0500

    Update evaluate-manager.js

--- api/evaluate-manager.js
+++ api/evaluate-manager.js
@@ -51,7 +51,7 @@ export default async function handler(req, res) {
     ];
 
     const model = genAI.getGenerativeModel({ 
-        model: "gemini-1.5-flash-latest",
+        model: "gemini-1.5-pro-latest",
         safetySettings,
         generationConfig: { responseMimeType: "application/json" } 
     });
