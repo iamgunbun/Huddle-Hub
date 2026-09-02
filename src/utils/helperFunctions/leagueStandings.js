a9337df966e45398afa4d97a1062c86a317e91a9
iamgunbun <ammonsgunner@gmail.com>
2026-09-02 15:05:57 -0500

    another one

--- src/utils/helperFunctions/leagueStandings.js
+++ src/utils/helperFunctions/leagueStandings.js
@@ -1,43 +1,48 @@
-import { leagueID } from '$lib/utils/leagueInfo';
-import { getNflState } from "./nflState"
-import { getLeagueData } from "./leagueData"
-import { getLeagueRosters } from "./leagueRosters"
+import { leagueID as defaultLeagueID } from '$lib/utils/leagueInfo';
+import { getNflState } from "./nflState";
+import { getLeagueData } from "./leagueData";
+import { getLeagueRosters } from "./leagueRosters";
 import { waitForAll } from './multiPromise';
 import { get } from 'svelte/store';
-import {standingsStore} from '$lib/stores';
+import { standingsStore } from '$lib/stores';
+import { activeLeague } from '../helper'; // or wherever activeLeague store is located
 import { round } from './universalFunctions';
 
-export const getLeagueStandings = async () => {
-	if(get(standingsStore).standingsInfo) {
-		return get(standingsStore);
-	}
+export const getLeagueStandings = async (queryLeagueID = null) => {
+    let id = queryLeagueID;
+    if (!id) {
+        id = defaultLeagueID;
+    }
 
-	const [nflState, leagueData, rostersData] = await waitForAll(
-		getNflState(),
-		getLeagueData(),
-		getLeagueRosters(),
-	).catch((err) => { console.error(err); });
+    const store = get(standingsStore);
+    if(store && store.standingsInfo && store.league_id === id) {
+        return store;
+    }
 
-	const yearData = leagueData.season;
-	const regularSeasonLength = leagueData.settings.playoff_week_start - 1;
-	const divisions = leagueData.settings.divisions && leagueData.settings.divisions > 1;
-    const rosters = rostersData.rosters;
+    const [nflState, leagueData, rostersData] = await waitForAll(
+        getNflState(),
+        getLeagueData(id),
+        getLeagueRosters(id),
+    ).catch((err) => { console.error(err); });
 
-	// if the season hasn't started, standings can't be created
-	if((leagueData.status != "in_season" && leagueData.status != "post_season" && leagueData.status != "complete") || nflState.week < 1) {
-		return null;
-	}
+    if (!leagueData || !rostersData) return null;
 
-	let standings = {};
+    const yearData = leagueData.season;
+    const regularSeasonLength = leagueData.settings?.playoff_week_start ? leagueData.settings.playoff_week_start - 1 : 14;
+    const divisions = leagueData.settings?.divisions && leagueData.settings.divisions > 1;
+    const rosters = rostersData.rosters || {};
+
+    let standings = {};
     for(const rosterID in rosters) {
         const roster = rosters[rosterID];
+        const settings = roster.settings || {};
         standings[rosterID] = {
             rosterID,
-            wins: roster.settings.wins,
-            losses: roster.settings.losses,
-            ties: roster.settings.ties,
-            fpts: round(roster.settings.fpts + (roster.settings.fpts_decimal / 100)),
-            fptsAgainst: round(roster.settings.fpts_against + (roster.settings.fpts_against_decimal / 100)),
+            wins: settings.wins || 0,
+            losses: settings.losses || 0,
+            ties: settings.ties || 0,
+            fpts: round((settings.fpts || 0) + ((settings.fpts_decimal || 0) / 100)),
+            fptsAgainst: round((settings.fpts_against || 0) + ((settings.fpts_against_decimal || 0) / 100)),
             streak: roster.metadata?.streak || 0,
             divisionWins: divisions ? 0 : null,
             divisionLosses: divisions ? 0 : null,
@@ -45,89 +50,13 @@ export const getLeagueStandings = async () => {
         }
     }
 
-    if(divisions) {
-        let week = 0;
-        if(nflState.season_type == 'regular') {
-            // max the week out at end of regular season
-            week = nflState.display_week > regularSeasonLength ? regularSeasonLength + 1 : nflState.display_week;
-        } else if(nflState.season_type == 'post') {
-            week = regularSeasonLength + 1;
-        }
-
-        // if at least one week hasn't been completed, then standings can't be created
-        if(week < 2) {
-            return null;
-        }
-
-        // pull in all matchup data for the season
-        const matchupsPromises = [];
-        for(let i = week - 1; i > 0; i--) {
-            matchupsPromises.push(fetch(`https://api.sleeper.app/v1/league/${leagueID}/matchups/${i}`, {compress: true}))
-        }
-        const matchupsRes = await waitForAll(...matchupsPromises);
-
-        // convert the json matchup responses
-        const matchupsJsonPromises = [];
-        for(const matchupRes of matchupsRes) {
-            const data = matchupRes.json();
-            matchupsJsonPromises.push(data)
-            if (!matchupRes.ok) {
-                throw new Error(data);
-            }
-        }
-        const matchupsData = await waitForAll(...matchupsJsonPromises).catch((err) => { console.error(err); }).catch((err) => { console.error(err); });
-
-        // process all the matchups
-        for(const matchup of matchupsData) {
-            standings = processStandings(matchup, standings, rosters);
-        }
+    const response = {
+        standingsInfo: standings,
+        yearData,
+        league_id: id
     }
+    
+    standingsStore.update(() => response);
 
-	const response = {
-		standingsInfo: standings,
-		yearData,
-	}
-	
-	standingsStore.update(() => response);
-
-	return response;
-}
-
-const processStandings = (matchup, standingsData, rosters) => {
-	const matchups = {};
-	for(const match of matchup) {
-		if(!matchups[match.matchup_id]) {
-			matchups[match.matchup_id] = [];
-		}
-		const rosterID = match.roster_id;
-
-		matchups[match.matchup_id].push({
-			rosterID,
-			division: rosters[rosterID].settings.division,
-			points: match.points,
-		})
-	}
-
-	for(const matchupKey in matchups) {
-		const teamA = matchups[matchupKey][0];
-		const teamB = matchups[matchupKey][1];
-	
-		const divisionMatchup = teamA.division && teamB.division && teamA.division == teamB.division;
-
-        if(divisionMatchup) {
-            if(teamA.points > teamB.points) {
-                standingsData[teamA.rosterID].divisionWins ++;
-                standingsData[teamB.rosterID].divisionLosses ++;
-                continue;
-            } else if(teamB.points > teamA.points) {
-                standingsData[teamB.rosterID].divisionWins ++;
-                standingsData[teamA.rosterID].divisionLosses ++;
-                continue;
-            } else {
-                standingsData[teamA.rosterID].divisionTies ++;
-                standingsData[teamB.rosterID].divisionTies ++;
-            }
-        }
-	}
-	return standingsData;
+    return response;
 }
